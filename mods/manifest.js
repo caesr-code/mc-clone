acc2ebaf0c66c9ef9d77e61da59cd8edd3a2fd85
caesr-code <dungeonsndumbins@gmail.com>
2026-07-21 15:33:28 +1000

    Delete mods directory

--- mods/manifest.js
+++
@@ -1,4 +0,0 @@
-// Add mod script paths here. They load in order before the game starts.
-// Example:
-// window.VOIDLANDS_EXTERNAL_MODS = ['mods/example-mod.js'];
-window.VOIDLANDS_EXTERNAL_MODS = [];
